72414079a0b21c0bd591a70c50a3bf7632183bf8
ricksterhd123 <ricksterhd123@gmail.com>
2024-08-05 03:18:25 +0100

    simplify build process

---
+++ scripts/install-raylib.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+
+set -e
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BUILD_DIR=$SCRIPT_DIR/../build
+
+mkdir -p $BUILD_DIR
+pushd $BUILD_DIR
+
+if [ ! -d $BUILD_DIR/raylib ]; then
+    git clone --depth 1 https://github.com/raysan5/raylib.git raylib
+fi
+
+pushd ./raylib/build
+cmake -DBUILD_SHARED_LIBS=ON ..
+make
+sudo make install
+popd
+popd
